4aa019155abbe220174cb8321d693af2e4a275ce
lost_mohican <erkinun@gmail.com>
2014-09-19 18:23:48 +0300

    base files and main class

---
+++ src/main/java/TrainSimulation.java
@@ -0,0 +1,9 @@
+/**
+ * Created by ERKIN on 19/09/2014.
+ */
+public class TrainSimulation {
+
+    public static void main(String[] args) {
+        System.out.println("Starting train simulation in a couple of commits");
+    }
+}
